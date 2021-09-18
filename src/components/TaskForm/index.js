0bba492eb0c09bad435ed1ae8fd07b9a9ad20b28
Larsen Mo <BehemothOz@yandex.ru>
2021-09-18 22:37:11 +0300

    update task form

--- src/components/TaskForm/index.js
+++ src/components/TaskForm/index.js
@@ -1,7 +1,19 @@
 import { Children } from 'react';
-import { TextField, Paper, Button } from '@mui/material';
+import {
+    TextField,
+    Paper,
+    Button,
+    FormControl,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormHelperText,
+    Stack,
+} from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
 import { Link as RouterLink, useHistory } from 'react-router-dom';
+import DatePicker from '@mui/lab/DatePicker';
+import Box from '@mui/material/Box';
 
 // const useStyles = makeStyles(theme => ({
 //     paper: {
@@ -17,6 +29,13 @@ import { Link as RouterLink, useHistory } from 'react-router-dom';
     плановое время окончания
     фактическое время начала
     фактическое время окончания
+
+    title
+    type
+    plannedStartTime
+    plannedEndTime
+    actualStartTime
+    actualEndTime
 */
 
 export const TaskForm = () => {
@@ -30,70 +49,117 @@ export const TaskForm = () => {
     };
 
     return (
-        <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-                name="login"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                    <TextField
-                        label="title"
-                        fullWidth
-                        required
-                        error={false}
-                        style={{ marginBottom: 8 }}
-                        inputProps={{
-                            autoComplete: 'new-password',
-                            form: {
-                                autoComplete: 'off',
-                            },
+        <Box sx={{ p: 2 }}>
+            <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="title"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                        <TextField
+                            label="Title"
+                            fullWidth
+                            required
+                            error={false}
+                            style={{ marginBottom: 8 * 2 }}
+                            size="small"
+                            inputProps={{
+                                autoComplete: 'new-password',
+                                form: {
+                                    autoComplete: 'off',
+                                },
+                            }}
+                            {...field}
+                        />
+                    )}
+                />
+                <FormControl sx={{ marginBottom: 2 }} size="small" fullWidth>
+                    <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
+                    <Controller
+                        name="type"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => (
+                            <>
+                                <Select
+                                    label="Type"
+                                    {...field}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                                {/* <FormHelperText>With label + helper text</FormHelperText> */}
+                            </>
+                        )}
+                    />
+                </FormControl>
+                <Stack direction="row" spacing={2} sx={{ marginBottom: 2 }}>
+                    <Controller
+                        name="plannedStartTime"
+                        control={control}
+                        defaultValue={null}
+                        render={({ field }) => {
+                            return (
+                                <DatePicker
+                                    label="Planned Start Time"
+                                    renderInput={params => <TextField size="small" {...params} />}
+                                    {...field}
+                                />
+                            );
                         }}
-                        {...field}
                     />
-                )}
-            />
-            <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                    <TextField
-                        type="password"
-                        label="Password"
-                        fullWidth
-                        required
-                        error={false}
-                        style={{ marginBottom: 8 * 2 }}
-                        {...field}
+                    <Controller
+                        name="plannedEndTime"
+                        control={control}
+                        defaultValue={null}
+                        render={({ field }) => (
+                            <DatePicker
+                                label="Planned End Time"
+                                renderInput={params => <TextField size="small" {...params} />}
+                                {...field}
+                            />
+                        )}
                     />
-                )}
-            />
-            <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                    <TextField
-                        id="datetime-local"
-                        label="Next appointment"
-                        type="datetime-local"
-                        defaultValue="2017-05-24T10:30"
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        {...field}
+                </Stack>
+                <Stack direction="row" spacing={2} sx={{ marginBottom: 2 }}>
+                    <Controller
+                        name="actualStartTime"
+                        control={control}
+                        defaultValue={null}
+                        render={({ field }) => (
+                            <DatePicker
+                                label="Actual Start Time"
+                                renderInput={params => <TextField size="small" {...params} />}
+                                {...field}
+                            />
+                        )}
                     />
-                )}
-            />
-            <div style={{ display: 'flex' }}>
-                <Button variant="contained" color="primary" type="submit" style={{ marginRight: 8 }}>
-                    Save
-                </Button>
-                <Button variant="contained" color="secondary">
-                    Close
-                </Button>
-            </div>
-        </form>
+                    <Controller
+                        name="actualEndTime"
+                        control={control}
+                        defaultValue={null}
+                        render={({ field }) => (
+                            <DatePicker
+                                label="Actual End Time"
+                                renderInput={params => <TextField size="small" {...params} />}
+                                {...field}
+                            />
+                        )}
+                    />
+                </Stack>
+                <div style={{ display: 'flex' }}>
+                    <Button variant="contained" color="primary" type="submit" style={{ marginRight: 8 }}>
+                        Save
+                    </Button>
+                    <Button variant="contained" color="secondary">
+                        Close
+                    </Button>
+                </div>
+            </form>
+        </Box>
     );
 };
