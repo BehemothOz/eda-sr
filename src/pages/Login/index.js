6fd6160c5f1f8662306aa2cc3ae7749fdd2d917c
Larsen Mo <BehemothOz@yandex.ru>
2021-09-21 22:43:09 +0300

    update login | register

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,42 +1,10 @@
-import { Children } from 'react';
-import { TextField, Paper, Button } from '@mui/material';
-// import { makeStyles } from '@material-ui/core/styles';
 import { useForm, Controller } from 'react-hook-form';
 import { Link as RouterLink, useHistory } from 'react-router-dom';
+import { TextField, Paper, Button, Stack, Typography } from '@mui/material';
 
-// const useStyles = makeStyles(theme => ({
-//     paper: {
-//         padding: theme.spacing(2),
-//     },
-//     input: {},
-// }));
-
-const CenterScreen = props => {
-    const { children } = props;
-
-    let child;
-    if (process.env.NODE_ENV === 'development') {
-        try {
-            child = Children.only(children);
-        } catch (err) {
-            console.error('CenterScreen component render error. See Children.only(children)');
-            // TODO: throw new Error('Children.only(children) error');
-        }
-    } else {
-        child = Children.only(children);
-    }
-
-    return (
-        <div
-            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100vh' }}
-        >
-            {child}
-        </div>
-    );
-};
+import { CenterScreen } from '../../components/CenterScreen';
 
 export const LoginPage = () => {
-    const classes = {};
     const history = useHistory();
 
     const { control, handleSubmit } = useForm();
@@ -46,60 +14,66 @@ export const LoginPage = () => {
         history.push('/');
     };
 
-    // console.count('Render:LoginPage'); - count 2
-
     return (
         <CenterScreen>
-            <Paper className={classes.paper}>
+            <Paper sx={{ width: '100%', p: 2 }}>
+                <Typography gutterBottom variant="h6" sx={{ color: 'text.secondary' }}>
+                    Login
+                </Typography>
                 <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-                    <Controller
-                        name="login"
-                        control={control}
-                        defaultValue=""
-                        render={({ field }) => (
-                            <TextField
-                                label="Login"
-                                fullWidth
-                                required
-                                error={false}
-                                style={{ marginBottom: 8 }}
-                                inputProps={{
-                                    autoComplete: 'new-password',
-                                    form: {
-                                        autoComplete: 'off',
-                                    },
-                                }}
-                                {...field}
-                            />
-                        )}
-                    />
-                    <Controller
-                        name="password"
-                        control={control}
-                        defaultValue=""
-                        render={({ field }) => (
-                            <TextField
-                                type="password"
-                                label="Password"
-                                fullWidth
-                                required
-                                error={false}
-                                style={{ marginBottom: 8 * 2 }}
-                                {...field}
-                            />
-                        )}
-                    />
-                    <div style={{ display: 'flex' }}>
-                        <Button variant="contained" color="primary" type="submit" style={{ marginRight: 8 }}>
-                            Send
-                        </Button>
-                        <Button variant="contained" color="secondary" to="/register" component={RouterLink}>
-                            Register
-                        </Button>
-                        <Button color="primary" to="/password" component={RouterLink} style={{ marginLeft: 'auto' }}>
-                            Password
-                        </Button>
-                    </div>
+                    <Stack spacing={2}>
+                        <Controller
+                            name="login"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => (
+                                <TextField
+                                    label="Login"
+                                    fullWidth
+                                    required
+                                    error={false}
+                                    inputProps={{
+                                        autoComplete: 'new-password',
+                                        form: {
+                                            autoComplete: 'off',
+                                        },
+                                    }}
+                                    {...field}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => (
+                                <TextField
+                                    type="password"
+                                    label="Password"
+                                    fullWidth
+                                    required
+                                    error={false}
+                                    {...field}
+                                />
+                            )}
+                        />
+                        <Stack spacing={1} direction="row">
+                            <Button variant="contained" color="primary" type="submit">
+                                Send
+                            </Button>
+                            <Button variant="contained" color="secondary" to="/register" component={RouterLink}>
+                                Register
+                            </Button>
+                            <Button
+                                color="primary"
+                                to="/password"
+                                component={RouterLink}
+                                style={{ marginLeft: 'auto' }}
+                            >
+                                Password
+                            </Button>
+                        </Stack>
+                    </Stack>
                 </form>
             </Paper>
         </CenterScreen>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,3 +1,73 @@
+import { useForm, Controller } from 'react-hook-form';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
+import { TextField, Paper, Button, Stack, Typography } from '@mui/material';
+
+import { CenterScreen } from '../../components/CenterScreen';
+
 export const RegisterPage = () => {
-    return 'Register Page';
+    const history = useHistory();
+
+    const { control, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log('form data after submit: ', data);
+        history.push('/login');
+    };
+
+    return (
+        <CenterScreen>
+            <Paper sx={{ width: '100%', p: 2 }}>
+                <Typography gutterBottom variant="h6" sx={{ color: 'text.secondary' }}>
+                    Registration
+                </Typography>
+                <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                    <Stack spacing={2}>
+                        <Controller
+                            name="login"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => (
+                                <TextField
+                                    label="Login"
+                                    fullWidth
+                                    required
+                                    error={false}
+                                    inputProps={{
+                                        autoComplete: 'new-password',
+                                        form: {
+                                            autoComplete: 'off',
+                                        },
+                                    }}
+                                    {...field}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => (
+                                <TextField
+                                    type="password"
+                                    label="Password"
+                                    fullWidth
+                                    required
+                                    error={false}
+                                    {...field}
+                                />
+                            )}
+                        />
+                        <Stack spacing={1} direction="row">
+                            <Button variant="contained" color="primary" type="submit">
+                                Register
+                            </Button>
+                            <Button color="primary" to="/login" component={RouterLink} style={{ marginLeft: 'auto' }}>
+                                Back
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </form>
+            </Paper>
+        </CenterScreen>
+    );
 };
