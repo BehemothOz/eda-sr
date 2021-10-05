eddee2bd2f93ef87747a1b141fcf9abe45ad012c
Larsen Mo <BehemothOz@yandex.ru>
2021-10-05 22:28:22 +0300

    fix stack for form

--- src/pages/Home/components/TaskForm/index.js
+++ src/pages/Home/components/TaskForm/index.js
@@ -39,100 +39,100 @@ export const TaskForm = props => {
             <Typography gutterBottom variant="h5" textAlign="end">{`${isEdit ? 'Update' : 'Create'} task`}</Typography>
 
             <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-                <Controller
-                    name="title"
-                    control={control}
-                    defaultValue={title || ''}
-                    render={({ field }) => (
-                        <TextField
-                            label="Title"
-                            fullWidth
-                            required
-                            error={false}
-                            size="small"
-                            inputProps={{
-                                autoComplete: 'new-password',
-                                form: {
-                                    autoComplete: 'off',
-                                },
-                            }}
-                            {...field}
-                        />
-                    )}
-                />
-                <Controller
-                    name="type"
-                    control={control}
-                    defaultValue=""
-                    render={({ field }) => (
-                        <TypeSelect label="Type" size="small" fullWidth {...field} />
-                    )}
-                />
-
-                <Stack direction="row" spacing={2} sx={{ marginBottom: 2 }}>
+                <Stack spacing={2}>
                     <Controller
-                        name="plannedStartTime"
+                        name="title"
                         control={control}
-                        defaultValue={null}
-                        render={({ field }) => {
-                            return (
+                        defaultValue={title || ''}
+                        render={({ field }) => (
+                            <TextField
+                                label="Title"
+                                fullWidth
+                                required
+                                error={false}
+                                size="small"
+                                inputProps={{
+                                    autoComplete: 'new-password',
+                                    form: {
+                                        autoComplete: 'off',
+                                    },
+                                }}
+                                {...field}
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="type"
+                        control={control}
+                        defaultValue=""
+                        render={({ field }) => <TypeSelect label="Type" size="small" fullWidth {...field} />}
+                    />
+
+                    <Stack direction="row" spacing={2}>
+                        <Controller
+                            name="plannedStartTime"
+                            control={control}
+                            defaultValue={null}
+                            render={({ field }) => {
+                                return (
+                                    <DatePicker
+                                        label="Planned Start Time"
+                                        renderInput={params => <TextField size="small" {...params} />}
+                                        {...field}
+                                    />
+                                );
+                            }}
+                        />
+                        <Controller
+                            name="plannedEndTime"
+                            control={control}
+                            defaultValue={null}
+                            render={({ field }) => (
                                 <DatePicker
-                                    label="Planned Start Time"
+                                    label="Planned End Time"
                                     renderInput={params => <TextField size="small" {...params} />}
                                     {...field}
                                 />
-                            );
-                        }}
-                    />
-                    <Controller
-                        name="plannedEndTime"
-                        control={control}
-                        defaultValue={null}
-                        render={({ field }) => (
-                            <DatePicker
-                                label="Planned End Time"
-                                renderInput={params => <TextField size="small" {...params} />}
-                                {...field}
-                            />
-                        )}
-                    />
-                </Stack>
+                            )}
+                        />
+                    </Stack>
 
-                <Stack direction="row" spacing={2} sx={{ marginBottom: 2 }}>
-                    <Controller
-                        name="actualStartTime"
-                        control={control}
-                        defaultValue={null}
-                        render={({ field }) => (
-                            <DatePicker
-                                label="Actual Start Time"
-                                renderInput={params => <TextField size="small" {...params} />}
-                                {...field}
-                            />
-                        )}
-                    />
-                    <Controller
-                        name="actualEndTime"
-                        control={control}
-                        defaultValue={null}
-                        render={({ field }) => (
-                            <DatePicker
-                                label="Actual End Time"
-                                renderInput={params => <TextField size="small" {...params} />}
-                                {...field}
-                            />
-                        )}
-                    />
+                    <Stack direction="row" spacing={2}>
+                        <Controller
+                            name="actualStartTime"
+                            control={control}
+                            defaultValue={null}
+                            render={({ field }) => (
+                                <DatePicker
+                                    label="Actual Start Time"
+                                    renderInput={params => <TextField size="small" {...params} />}
+                                    {...field}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="actualEndTime"
+                            control={control}
+                            defaultValue={null}
+                            render={({ field }) => (
+                                <DatePicker
+                                    label="Actual End Time"
+                                    renderInput={params => <TextField size="small" {...params} />}
+                                    {...field}
+                                />
+                            )}
+                        />
+                    </Stack>
+
+                    <div style={{ display: 'flex' }}>
+                        <Button variant="contained" color="primary" type="submit" style={{ marginRight: 8 }}>
+                            {isEdit ? 'Update' : 'Create'}
+                        </Button>
+                        <Button variant="contained" color="secondary" onClick={onClose}>
+                            Close
+                        </Button>
+                    </div>
                 </Stack>
-                
-                <div style={{ display: 'flex' }}>
-                    <Button variant="contained" color="primary" type="submit" style={{ marginRight: 8 }}>
-                        {isEdit ? 'Update' : 'Create'}
-                    </Button>
-                    <Button variant="contained" color="secondary" onClick={onClose}>
-                        Close
-                    </Button>
-                </div>
             </form>
         </Box>
     );
