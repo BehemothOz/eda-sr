d189db0021a90758c346b358aec71231bd2e452e
Larsen Mo <BehemothOz@yandex.ru>
2021-10-06 17:02:38 +0300

    add time in filter

--- src/pages/Home/components/TaskFilter/index.js
+++ src/pages/Home/components/TaskFilter/index.js
@@ -1,8 +1,9 @@
-import { Stack, TextField } from '@mui/material';
+import { Stack, Container } from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
 
 import { TypeSelect } from 'components/inputs/TypeSelect';
 import { UserSelect } from 'components/inputs/UserSelect';
+import { DatePicker } from 'shared/DatePicker';
 
 export const TaskFilter = () => {
     const { control, handleSubmit } = useForm();
@@ -12,25 +13,41 @@ export const TaskFilter = () => {
     };
 
     return (
-        <div style={{ padding: 16, backgroundColor: '#fff' }}>
-            <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-                <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
-                    <Controller
-                        name="user"
-                        control={control}
-                        defaultValue=""
-                        render={({ field }) => <UserSelect label="User" size="small" fullWidth {...field} />}
-                    />
-                    <Controller
-                        name="type"
-                        control={control}
-                        defaultValue=""
-                        render={({ field }) => <TypeSelect label="Type" size="small" fullWidth {...field} />}
-                    />
-                    <TextField label="Outlined" variant="outlined" size="small" />
-                    <TextField label="Outlined" variant="outlined" size="small" />
-                </Stack>
-            </form>
+        <div style={{ padding: '16px 0', backgroundColor: '#fff' }}>
+            <Container maxWidth="lg">
+                <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                    <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+                        <Controller
+                            name="user"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => <UserSelect label="User" size="small" fullWidth {...field} />}
+                        />
+                        <Controller
+                            name="type"
+                            control={control}
+                            defaultValue=""
+                            render={({ field }) => <TypeSelect label="Type" size="small" fullWidth {...field} />}
+                        />
+                        <Controller
+                            name="a"
+                            control={control}
+                            defaultValue={null}
+                            render={({ field }) => {
+                                return <DatePicker label="Time 1" {...field} />;
+                            }}
+                        />
+                        <Controller
+                            name="b"
+                            control={control}
+                            defaultValue={null}
+                            render={({ field }) => {
+                                return <DatePicker label="Time 2" {...field} />;
+                            }}
+                        />
+                    </Stack>
+                </form>
+            </Container>
         </div>
     );
 };
