1c5613aef94f397914c1c64fe057c9efa3ce819b
Larsen Mo <BehemothOz@yandex.ru>
2021-10-18 18:16:23 +0300

    fix var

--- src/libs/test.js
+++ src/libs/test.js
@@ -3,15 +3,12 @@ const delay = ms =>
         setTimeout(resolve, ms);
     });
 
-const fetch = args => {
-    return new Promise((_, reject) => {
-        setTimeout(() => {
-            reject({ status: 413, payload: { name: 'Ivan' } });
-        }, 1000);
-    });
-};
+/*
+ * repeatable function
+ * error response: { status: 413, payload: {} }
+ */
 
-const repeatable = (func, options = {}) => {
+export const repeatable = (func, options = {}) => {
     const { duration = 2000, max = 3, statusCodes = [413], onError = null } = options;
 
     let count = 0;
@@ -19,41 +16,18 @@ const repeatable = (func, options = {}) => {
     const retry = async args => {
         try {
             return await func(...args);
-        } catch (e) {
-            if (count < max && statusCodes.includes(e.status)) {
-                onError && onError(e);
+        } catch (error) {
+            if (count < max && statusCodes.includes(error.status)) {
+                onError && onError(error);
 
                 delay(duration);
                 count += 1;
                 return retry(args);
             }
 
-            throw e;
+            throw error;
         }
     };
 
     return (...args) => retry(args);
 };
-
-/*
-    For testing
-*/
-const search = async () => {
-    const asyncSearch = fetch;
-
-    const repeatableSearch = repeatable(asyncSearch, {
-        max: 3,
-        onError: () => {
-            console.log('Ooops! Timeout error.');
-        },
-    });
-
-    try {
-        const rr = await repeatableSearch({ a: 100, b: 200 });
-        console.log('Look here', rr);
-    } catch (e) {
-        console.error(e);
-    }
-};
-
-search();
