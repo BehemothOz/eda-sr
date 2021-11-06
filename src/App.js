041f0942f0b775a65d8f9d24116513e0f04ec61f
Larsen Mo <BehemothOz@yandex.ru>
2021-11-06 16:45:45 +0300

    routers to app component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import Button from '@material-ui/core/Button';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <Button color="primary">asd</Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { LoginPage } from './pages/Login';
+import { RegisterPage } from './pages/Register';
+import { PasswordPage } from './pages/Password';
+import { SecretPasswordPage } from './pages/Password/Secret';
+import { RestorePasswordPage } from './pages/Password/Restore';
+import { HomePage } from './pages/Home';
+import { ProfilePage } from './pages/Profile';
+
+export const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+                <Route path="/profile">
+                    <ProfilePage />
+                </Route>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route exact path="/register">
+                    <RegisterPage />
+                </Route>
+                <Route exact path="/password">
+                    <PasswordPage />
+                </Route>
+                <Route exact path="/password/secret">
+                    <SecretPasswordPage />
+                </Route>
+                <Route exact path="/password/restore">
+                    <RestorePasswordPage />
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
