7a3728212cfceece12c0e90c5d1edd8d38d2bf55
Larsen Mo <BehemothOz@yandex.ru>
2021-11-14 16:26:49 +0300

    mock data (temp)

---
+++ src/api/services/__mock.js
@@ -0,0 +1,26 @@
+export const initialUser = [
+    [
+        '000-S',
+        {
+            id: '000-S',
+            login: 'some@sm.bg',
+            firstName: '',
+            middleName: '',
+            lastName: '',
+            secretQuestion: 1,
+            secretAnswer: '2',
+        },
+    ],
+];
+
+export const initialTasks = [
+    {
+        id: 1,
+        title: 'Cook pasta with chicken',
+        type: '',
+        plannedStartTime: new Date(2021, 9, 1),
+        plannedEndTime: new Date(2021, 9, 25),
+        actualStartTime: '',
+        actualEndTime: '',
+    },
+];
