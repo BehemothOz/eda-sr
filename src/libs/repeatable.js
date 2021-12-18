4186d91d24e02127de4bf5da20f2d26684d44065
Larsen Mo <BehemothOz@yandex.ru>
2021-12-18 15:38:12 +0300

    update repeatable

--- src/libs/repeatable.js
+++ src/libs/repeatable.js
@@ -3,22 +3,24 @@ const delay = ms =>
         setTimeout(resolve, ms);
     });
 
-/*
- * repeatable function
- * error response: { status: 413, payload: {} }
- */
+export const repeatable = (asyncFunc, options = {}) => {
+    // if (asyncFunc[Symbol.toStringTag] !== 'AsyncFunction') {
+    //     throw new Error('The first argument must be asynchronous function');
+    // }
 
-export const repeatable = (func, options = {}) => {
-    const { duration = 2000, max = 3, statusCodes = [413], onError = null } = options;
+    const { duration = 2000, max = 3, statusCodes = [408], onError = null } = options;
 
+    let isRunning = true;
     let count = 0;
 
     const retry = async args => {
         try {
-            return await func(...args);
+            return await asyncFunc(...args);
         } catch (error) {
-            if (count < max && statusCodes.includes(error.status)) {
-                onError && onError(error);
+            if (isRunning && max !== 0 && count < max && statusCodes.includes(error.status)) {
+                // if (max !== 0 && count < max) {
+                console.log(error.status);
+                onError && onError({ _retryCount: count, error });
 
                 await delay(duration);
                 count += 1;
@@ -29,5 +31,11 @@ export const repeatable = (func, options = {}) => {
         }
     };
 
-    return (...args) => retry(args);
+    const repeat = (...args) => retry(args);
+    repeat.clear = () => {
+        console.log('STOP repeat')
+        isRunning = false
+    };
+
+    return repeat;
 };
