0c943fe75453d9f5557ce8d721329c5c412680cf
Larsen Mo <BehemothOz@yandex.ru>
2022-01-07 18:26:39 +0300

    check connections

--- public/shared.worker.js
+++ public/shared.worker.js
@@ -1,9 +1,10 @@
-onconnect = function (e) {
-    console.log(e)
-    // var port = e.ports[0];
+const connections = [];
+
+self.addEventListener('connect', function (e) {
+    connections.push(`item ${connections.length}`);
+    const port = e.ports[0];
 
     port.onmessage = function (e) {
-        var workerResult = 'Result: ' + e.data[0] * e.data[1];
-        port.postMessage('123123123123');
+        port.postMessage(`${connections.join(', ')}`);
     };
-};
+});
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -6,32 +6,38 @@ export const TestPage = () => {
     const [count, setCount] = useState(0);
     const workerRef = useRef();
 
+    const [s, ss] = useState('');
+
     useEffect(() => {
-        console.log(getPathFromPublic('shared.worker.js'))
+        console.log(getPathFromPublic('shared.worker.js'));
         const worker = new SharedWorker(getPathFromPublic('shared.worker.js'));
-        console.dir(worker);
         workerRef.current = worker;
 
         worker.port.addEventListener(
             'message',
             e => {
                 console.log(e.data);
+                ss(e.data);
             },
             false
         );
 
         worker.port.start();
+        // worker.port.postMessage("start");
+
+        return () => {
+            // worker.terminate()
+        };
     }, []);
 
     const sendToWorker = () => {
-        workerRef.current.port.postMessage(count);
-        setCount(p => p + 1);
+        workerRef.current.port.postMessage(count + 1);
     };
 
     return (
         <div>
             <button onClick={sendToWorker}>BTN FOR INC</button>
-            <span style={{ margin: '0 16px' }}>Result: {count}</span>
+            <span style={{ margin: '0 16px' }}>Result: {s}</span>
         </div>
     );
 };
