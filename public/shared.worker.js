4e0aadc49ff4ed73697c7614fae8f41d9a9075f6
Larsen Mo <BehemothOz@yandex.ru>
2022-01-15 14:25:58 +0300

    new struct

--- public/shared.worker.js
+++ public/shared.worker.js
@@ -17,6 +17,50 @@ class Data {
 
 const state = new Data();
 
+class Tasks {
+    constructor() {
+        this.tasks = initialTasks;
+    }
+
+    getAll() {
+        return this.tasks;
+        // throw new Error('Ooops')
+    }
+
+    getByID() {}
+
+    create(data) {
+        const id = generateID.get();
+        this.tasks = [...this.tasks, { id, ...data }];
+
+        return id;
+        // throw new Error('Ooops')
+    }
+
+    update(id, data) {
+        this.tasks = this.tasks.map(task => (task.id === id ? { ...task, ...data } : task));
+        return id;
+    }
+
+    delete(id) {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+        return id;
+    }
+}
+
+/*
+    getAll
+    create
+    update
+    delete
+
+    function send(payload) {
+        const { type, data } = payload;
+        const newData = data;
+        return { type, data: newData }
+    }
+*/
+
 self.addEventListener('connect', function (e) {
     const port = e.ports[0];
 
